e469d55e3516c740da34fc04166444783d688471
Dillon Powers <dillon.c.powers@gmail.com>
2017-02-10 11:58:30 -0500

    file structure refactor

--- server/api.js
+++ server/api.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const utils = require('./serverUtils');
+const utils = require('./utils');
 
 
 // constants for calculating data of however many years
---
+++ server/index.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const express = require('express');
+const volleyball = require('volleyball');
+const bodyParser = require('body-parser');
+const api = require('./api');
+const { resolve } = require('path'); 
+
+const app = express();
+
+app.use(volleyball);
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use('/public', express.static(resolve(__dirname, '../public')));
+
+app.use('/api', api);
+
+app.get('/', (req, res, next) => {
+	res.sendFile(resolve(__dirname, '../index.html'));
+});
+
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+  console.error(`CAUGHT ERROR ${err}`);
+  res.json(err);
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
+
